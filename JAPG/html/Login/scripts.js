fa7501d72036259e5b9453aca174f9925940f438
Jose Antonio Padre Garcia <padre_japg@163.com>
2024-08-21 17:57:01 -0600

    p1

--- JAPG/html/Login/scripts.js
+++ JAPG/html/Login/scripts.js
@@ -1,19 +1,27 @@
-function openTab(tabName) {
-    const tabs = document.querySelectorAll('.tab-content');
-    const buttons = document.querySelectorAll('.tab-button');
-
-    tabs.forEach(tab => {
-        tab.classList.remove('active');
-    });
-
-    buttons.forEach(button => {
-        button.classList.remove('active');
-    });
-
-    document.getElementById(tabName).classList.add('active');
-    event.target.classList.add('active');
+// Función para abrir el modal
+function openModal() {
+    document.getElementById("modal").style.display = "flex";
 }
 
-function scrollToForm() {
-    document.getElementById('login-register').scrollIntoView({ behavior: 'smooth' });
+// Función para cerrar el modal
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+// Función para cambiar entre las pestañas de Iniciar Sesión y Registrarse
+function openTab(tabName) {
+    var i;
+    var tabcontent = document.getElementsByClassName("tab-content");
+    var tabbuttons = document.getElementsByClassName("tab-button");
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].className = tabcontent[i].className.replace(" active", "");
+    }
+
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).className += " active";
+    event.currentTarget.className += " active";
 }
