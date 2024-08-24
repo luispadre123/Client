644155cff53f2fea7f14d30617f46b0b4899544b
Luis Antonio Padre García <luisgarcia.lapg@gmail.com>
2024-08-23 19:21:43 -0600

    Se Agrega estructura inicial

---
+++ src/renderer/src/components/Auth.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Login from '../views/Login';
+import Register from '../views/Register';
+import CustomTitleBar from './CustomTitleBar';
+
+
+const Auth: React.FC = () => {
+    const [isLogin, setIsLogin] = useState(true);
+
+    const toggleAuthMode = () => {
+        setIsLogin(!isLogin);
+    };
+
+    return (
+        <>
+            <CustomTitleBar />
+            <div className="ui middle aligned center aligned grid">
+                <div className="column" style={{ maxWidth: 450 }}>
+                    {isLogin ? (
+                        <>
+                            <Login >
+                            <div className="ui message">
+                                ¿Nuevo aquí? <a href="#" onClick={toggleAuthMode}>Regístrate</a>
+                            </div>
+                            </Login>
+                            
+                        </>
+                    ) : (
+                        <>
+                            <Register >
+                            <div className="ui message">
+                                ¿Ya tienes una cuenta? <a href="#" onClick={toggleAuthMode}>Inicia Sesión</a>
+                            </div>
+                            </Register>
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Auth;
---
+++ src/renderer/src/context/index.tsx
@@ -0,0 +1,25 @@
+import AuthContext from './Auth.Context'
+
+import Home from '../views/Home';
+import Login from '../views/Login';
+import Register from '../views/Register';
+import CustomTitleBar from '../components/CustomTitleBar';
+import Auth from '../components/Auth';
+import { ApolloProvider } from '@apollo/client';
+import client from '../api/apolloClient';
+
+// Detecta la vista basada en el parámetro de búsqueda de la URL
+const searchParams = new URLSearchParams(window.location.search);
+const view = searchParams.get('view');
+
+
+export default function () {
+    console.log(view,'view')
+    return (
+        <ApolloProvider client={client}>
+        <AuthContext>
+            <Auth/>
+        </AuthContext>
+        </ApolloProvider>
+    )
+}
