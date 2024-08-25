7b2590863190e70e8564fa57e6da8602dbe6a7a2
Jose Antonio Padre Garcia <padre_japg@163.com>
2024-08-25 17:00:24 -0600

    fix GameLobby size

--- src/renderer/src/router/index.tsx
+++ src/renderer/src/router/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "../components/Auth";
 import GameLobby from "../views/Loby";
@@ -8,6 +8,17 @@ import {useAuth} from "../context/Auth.Context.tsx";
 const AppRouter: React.FC = () => {
     const { data } = useAuth();  // Aquí `useAuth` está seguro porque `AppRouter` está envuelto en `AuthContextProvider`
 
+    console.log(data, "data");
+  
+    useEffect(() => {
+      if (window.api.resizeWindow) {
+        if (data.token && data.token !== "undefined") {
+          window.api.resizeWindow(1100, 700);
+        } else {
+          window.api.resizeWindow(400, 480); // Cambia el tamaño para no autenticados
+        }
+      }
+    }, [data.token]);
     return (
         <Router>
             <Routes>
